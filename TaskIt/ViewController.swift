db09d977a507eeca68d64b4fbd15509385204f69
Vincent van Leeuwen <vincentvanleeuwen@vincentleeuwen.local>
2014-12-19 16:06:48 +0100

    Initial Commit

---
+++ TaskIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskIt
+//
+//  Created by Vincent van Leeuwen on 19/12/14.
+//  Copyright (c) 2014 Vincent van Leeuwen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
